c38ffe4fcec18e94049fed00f2a8e3b824dc3908
ansumiriam <ans.varghese@gmail.com>
2026-02-02 01:01:43 +0530

    fix(build): removed autoprefixer to fix build error and added vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/54DayNovena/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+      ],
     },
   },
 })
