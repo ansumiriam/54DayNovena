32b4cd90e22e8ea9b0a670c8ff8b6bf77d308616
ansumiriam <ans.varghese@gmail.com>
2026-02-02 01:34:45 +0530

    Updates

---
+++ assets/index-DsKuhC35.js
@@ -0,0 +1,1 @@
+import{g as r,d as t}from"./index-BFDHgzF9.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/54DayNovena/sw.js', { scope: '/54DayNovena/' })})}
